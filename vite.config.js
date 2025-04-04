1e49e348d589cfa5a72e91fa05d5fe1304f2200f
capybaraman777 <s108d221@gm.hnvs.cy.edu.tw>
2025-04-05 03:57:27 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cpaysearch/', // <-- 這裡請記得改成你的 GitHub repository 名稱
 })
