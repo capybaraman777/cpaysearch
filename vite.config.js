446ced706cddf68e0daf60c02e1caf6a0f9ca044
capybaraman777 <s108d221@gm.hnvs.cy.edu.tw>
2025-04-10 23:46:10 +0800

    在上傳圖片

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cpaysearch/', // <-- 這裡請記得改成你的 GitHub repository 名稱
-})
+  base: '/cpaysearch/', // <-- GitHub Pages 路徑
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react';
+            if (id.includes('react-icons')) return 'icons';
+            if (id.includes('lodash')) return 'lodash';
+            if (id.includes('axios')) return 'axios';
+            return 'vendor'; // 其他第三方包
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // optional: 提高警告門檻（不影響功能）
+  }
+});
